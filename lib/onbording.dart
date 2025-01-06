36c49a3c10b285f95ababf08b7f53ec121d53bf7
mhmiripoor <mhmiripoor@gmail.com>
2025-01-07 00:26:55 +0330

    create onboarding page

---
+++ lib/onbording.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Onboarding extends StatelessWidget {
+  const Onboarding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      color: Colors.purpleAccent,
+    );
+  }
+}
